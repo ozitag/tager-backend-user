7a4c828e3278fb11ef7697018a2b9bf97739f1b3
Vital Ozierski <v.ozierski@ozitag.com>
2021-10-19 14:56:19 +0300

    add validation throw if password is too weak

--- src/Features/PasswordRestore/CompleteRestorePasswordFeature.php
+++ src/Features/PasswordRestore/CompleteRestorePasswordFeature.php
@@ -5,11 +5,13 @@ namespace OZiTAG\Tager\Backend\User\Features\PasswordRestore;
 use Illuminate\Http\Resources\Json\JsonResource;
 use OZiTAG\Tager\Backend\Core\Features\Feature;
 use OZiTAG\Tager\Backend\Core\Resources\SuccessResource;
+use OZiTAG\Tager\Backend\User\Exceptions\UserPasswordPolicyException;
 use OZiTAG\Tager\Backend\User\Jobs\PasswordRestore\GetTokenJob;
 use OZiTAG\Tager\Backend\User\Models\UserResetToken;
 use OZiTAG\Tager\Backend\User\Operations\RestoreUserPasswordOperation;
 use OZiTAG\Tager\Backend\User\Requests\PasswordRestore\CompletePasswordRestoreRequest;
 use OZiTAG\Tager\Backend\User\Utils\TagerUserConfig;
+use OZiTAG\Tager\Backend\Validation\Facades\Validation;
 
 class CompleteRestorePasswordFeature extends Feature
 {
@@ -29,10 +31,14 @@ class CompleteRestorePasswordFeature extends Feature
 
         $user = $token->user;
 
-        $this->run(RestoreUserPasswordOperation::class, [
-            'token' => $token,
-            'password' => $request->get('password')
-        ]);
+        try {
+            $this->run(RestoreUserPasswordOperation::class, [
+                'token' => $token,
+                'password' => $request->get('password')
+            ]);
+        } catch (UserPasswordPolicyException $exception) {
+            Validation::throw('password', $exception->getMessage());
+        }
 
         return new JsonResource([
             'email' => $user->email
--- src/Features/PasswordRestore/CompleteRestorePasswordWithCodeFeature.php
+++ src/Features/PasswordRestore/CompleteRestorePasswordWithCodeFeature.php
@@ -4,11 +4,13 @@ namespace OZiTAG\Tager\Backend\User\Features\PasswordRestore;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use OZiTAG\Tager\Backend\Core\Features\Feature;
+use OZiTAG\Tager\Backend\User\Exceptions\UserPasswordPolicyException;
 use OZiTAG\Tager\Backend\User\Jobs\PasswordRestore\GetTokenJob;
 use OZiTAG\Tager\Backend\User\Models\UserResetToken;
 use OZiTAG\Tager\Backend\User\Operations\RestoreUserPasswordOperation;
 use OZiTAG\Tager\Backend\User\Requests\PasswordRestore\CompletePasswordRestoreWithCodeRequest;
 use OZiTAG\Tager\Backend\User\Utils\TagerUserConfig;
+use OZiTAG\Tager\Backend\Validation\Facades\Validation;
 
 class CompleteRestorePasswordWithCodeFeature extends Feature
 {
@@ -25,10 +27,14 @@ class CompleteRestorePasswordWithCodeFeature extends Feature
 
         $user = $token->user;
 
-        $this->run(RestoreUserPasswordOperation::class, [
-            'token' => $token,
-            'password' => $request->password
-        ]);
+        try {
+            $this->run(RestoreUserPasswordOperation::class, [
+                'token' => $token,
+                'password' => $request->password
+            ]);
+        } catch (UserPasswordPolicyException $exception) {
+            Validation::throw('password', $exception->getMessage());
+        }
 
         return new JsonResource([
             'email' => $user->email
